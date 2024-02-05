01be6d855ecc9b2460a0a904c15011dbbce38aa7
izzyLim <izzy253@gmail.com>
2024-02-06 01:27:48 +0900

    2024/02/06 commit

--- gogglekaap/__init__.py
+++ gogglekaap/__init__.py
@@ -1,24 +1,37 @@
 from flask import Flask
 from flask import render_template
 from flask_wtf.csrf import CSRFProtect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 csrf = CSRFProtect()
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     print('run: create_app()')
     app = Flask(__name__)
     
     app.config['SECRET_KEY'] = 'secretkey'
-      
-    
+    app.config['SESSION_COOKIE_NAME'] = 'gogglekaap'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/gogglekaap?charset=utf8'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@3.35.27.89:3306/gogglekaap?charset=utf8'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        
     if app.config['DEBUG']:
         app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1 #디버깅 모드에서 캐시 제거
-        
+    
+    '''DB INIT'''
+    db.init_app(app)
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith('sqlite'):
+        migrate.init_app(app, db, render_as_batch=True)
+    else :
+        migrate.init_app(app, db)
+    
     '''ROUTE INIT'''
     from gogglekaap.routes import base_route, auth_route
     app.register_blueprint(base_route.bp)
     app.register_blueprint(auth_route.bp)
-
                 
     '''CSRF INIT'''
     csrf.init_app(app)
--- gogglekaap/routes/auth_route.py
+++ gogglekaap/routes/auth_route.py
@@ -1,24 +1,57 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, session, request
 from gogglekaap.forms.auth_form import LoginForm, RegisterForm
+from gogglekaap.models.user import User as UserModel
+from gogglekaap import db
+from werkzeug import security
 
 NAME = 'auth'
 bp = Blueprint(NAME, __name__, url_prefix='/auth')
+
+"""only for testing"""
+from dataclasses import dataclass
+
+
+@dataclass
+class User:
+    user_id: str
+    user_name: str
+    password: str
+
+USERS = []
+USERS.append(User('hidekuma', 'hidekuma', security.generate_password_hash('1234')))
+USERS.append(User('admin', 'admin', security.generate_password_hash('1234')))
+USERS.append(User('tester', 'tester', security.generate_password_hash('1234')))
 
 @bp.route('/')
 def index():
     return redirect(url_for(f'{NAME}.login'))
 
+
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        
         user_id = form.data.get('user_id')
         password = form.data.get('password')
-        return f'{user_id}, {password}'
+       
+        #user = [user for user in USERS if user.user_id == user_id]
+        user = UserModel.find_one_by_user_id(user_id)
+        if user:
+            #user = user[0]
+            if not security.check_password_hash(user.password, password):
+                flash('Password is not valid')
+            else:
+                session['user_id'] = user_id
+                return redirect(url_for('base.index'))
+        else:
+            flash('User ID does not exist') 
+                       
+
     else: 
         flash_form_errors(form)
     return render_template(f'{NAME}/login.html', form=form)
+
+
     
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
@@ -29,16 +62,33 @@ def register():
         user_name = form.data.get('user_name')
         password = form.data.get('password')
         repassword = form.data.get('repassword')
+        #user = [user for user in USERS if user.user_id == user_id]
+        user = UserModel.find_one_by_user_id(user_id)
+        if user:
+            flash('User ID is already exists')
+            return redirect(request.path)
+        else:
+            db.session.add(
+                UserModel(
+                    user_id = user_id,
+                    user_name = user_name,
+                    password = security.generate_password_hash(password)
+                )
+            )
+            db.session.commit()
+            #db.session.close()
+            session['user_id'] = user_id
+            return redirect(url_for('base.index'))
 
-        return f'{user_id}, {user_name}, {password}, {repassword}'
+
     else: 
         flash_form_errors(form)
     return render_template(f'{NAME}/register.html', form=form)
 
-@bp.route(f'{NAME}/logout')
+@bp.route('/logout')
 def logout():
-    form = RegisterForm()
-    return 'logout'
+    session.pop('user_id', None)
+    return redirect(url_for(f'{NAME}.login'))
 
 def flash_form_errors(form):
     for _, errors in form.errors.items():
